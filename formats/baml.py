4ac3d9a9452c1657130b92eaa91c2876ddaa81f8
Mohamed <mohamedelarchaoui766@gmail.com>
2026-03-11 11:48:26 +0100

    Update baml.py

--- formats/baml.py
+++ formats/baml.py
@@ -2,12 +2,9 @@ python -c "
 import pdfplumber
 p = r'T:\Dallas\CSS-BOC\CTL\REC\Réconciliation dérivés listés\FUND CHANNEL\2026\BAML\MSIM_SG_EOD Daily Statement.pdf'
 with pdfplumber.open(p) as pdf:
-    for i, page in enumerate(pdf.pages):
+    for i, page in enumerate(pdf.pages[:5]):
         texte = page.extract_text() or ''
-        if 'OPEN POSITIONS' in texte:
-            print(f'--- PAGE {i+1} ---')
-            for j, ligne in enumerate(texte.split('\n')):
-                print(f'[{j}] {ligne}')
-            print()
-            break  # juste la premiere page OPEN POSITIONS
+        print(f'--- PAGE {i+1} ---')
+        print(repr(texte[:500]))
+        print()
 "
