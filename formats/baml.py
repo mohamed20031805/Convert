c7046a3cb38a3598249f5a2249d0f866c8b30d12
Mohamed <mohamedelarchaoui766@gmail.com>
2026-03-11 11:52:00 +0100

    Update baml.py

--- formats/baml.py
+++ formats/baml.py
@@ -2,9 +2,10 @@ python -c "
 import pdfplumber
 p = r'T:\Dallas\CSS-BOC\CTL\REC\Réconciliation dérivés listés\FUND CHANNEL\2026\BAML\MSIM_SG_EOD Daily Statement.pdf'
 with pdfplumber.open(p) as pdf:
-    for i, page in enumerate(pdf.pages[:5]):
+    for i, page in enumerate(pdf.pages[1:4]):  # pages 2,3,4
         texte = page.extract_text() or ''
-        print(f'--- PAGE {i+1} ---')
-        print(repr(texte[:500]))
+        print(f'--- PAGE {i+2} ---')
+        for j, ligne in enumerate(texte.split('\n')):
+            print(f'[{j}] {ligne}')
         print()
 "
